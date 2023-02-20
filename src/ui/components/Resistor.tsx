6c9c30cc3e56fc73f9fff3d246ae6977a1090757
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2023-02-20 19:19:30 -0300

    fix: resistor width

--- src/ui/components/Resistor.tsx
+++ src/ui/components/Resistor.tsx
@@ -23,8 +23,7 @@ export function Resistor(props: ResistorProps) {
     return (
         <svg
             xmlns="http://www.w3.org/2000/svg"
-            width={200}
-            height={100}
+            viewBox="0 0 200 100"
             fill="none"
         >
             <path
