3792ba3828ec996efd9616eeb8eb9549512cfff1
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2023-02-27 16:58:40 -0300

    chore: add sass compiler config

---
+++ sass-compiler.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    generateMap: false,
+    formats: [
+        {
+            format: "compressed",
+            extensionName: ".css",
+        }
+    ],
+    excludeList: [
+        "**/node_modules/**",
+        ".vscode/**"
+    ],
+    autoprefix: []
+}
